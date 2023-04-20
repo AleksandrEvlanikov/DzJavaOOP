5ef21a5417a6d170bf2ec53b155f81832abcbcd3
Aleksandr <aleksandr.evlanicov@gmail.com>
2023-04-20 14:59:47 +0300

    Dz1

---
+++ src/Semminar2Oop/Obstacle.java
@@ -0,0 +1,20 @@
+package ru.geekbrains.task1;
+
+/**
+ * Препятствие
+ */
+public interface Obstacle {
+
+    /**
+     * Длина препятствия
+     * @return
+     */
+    int getLength();
+
+    /**
+     * Высота препятствия
+     * @return
+     */
+    int getHeight();
+
+}
